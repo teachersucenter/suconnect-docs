f0f87b781492ef000235f6c66111c81af61d8af1
ninnroot <misterjames.thiha@gmail.com>
2022-09-13 09:01:45 +0800

    cleaned up directories

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,13 +41,7 @@ const config = {
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: false,
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        // },
+       
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
@@ -65,12 +59,7 @@ const config = {
           src: "img/logosm.png",
         },
         items: [
-          {
-            type: "doc",
-            docId: "intro",
-            position: "left",
-            label: "Tutorial",
-          },
+
           {
             href: "https://github.com/facebook/docusaurus",
             label: "GitHub",
